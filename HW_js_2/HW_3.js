ca6cb35bb095662b66aaecebac8c64698fed6653
Klopovsky <95494364+Klopovsky@users.noreply.github.com>
2022-04-30 12:37:31 +0400

    Add files via upload

---
+++ HW_js_2/HW_3.js
@@ -0,0 +1,64 @@
+const enterprises = [
+    {
+      id: 1,
+      name: "Предприятие 1",
+      departments: [
+        {
+          id: 2,
+          name: "Отдел тестирования",
+          employees_count: 10,
+        },
+        {
+          id: 3,
+          name: "Отдел маркетинга",
+          employees_count: 20,
+        },
+        {
+          id: 4,
+          name: "Администрация",
+          employees_count: 15,
+        },
+      ]
+    },
+    {
+      id: 5,
+      name: "Предприятие 2",
+      departments: [
+        {
+          id: 6,
+          name: "Отдел разработки",
+          employees_count: 50,
+        },
+        {
+          id: 7,
+          name: "Отдел маркетинга",
+          employees_count: 20,
+        },
+        {
+          id: 8,
+          name: "Отдел охраны труда",
+          employees_count: 5,
+        },
+      ]
+    },
+    {
+      id: 9,
+      name: "Предприятие 3",
+      departments: [
+        {
+          id: 10,
+          name: "Отдел аналитики",
+          employees_count: 0,
+        },
+      ]
+    }
+  ]
+
+for (const key in enterprises) {
+    let quant = 0;
+    for (const k in enterprises[key].departments){
+        quant = quant + enterprises[key].departments[k].employees_count
+    }
+    console.log(enterprises[key].name + " (" + quant + " сотрудников)")
+    console.log(enterprises[key].departments)
+}  
